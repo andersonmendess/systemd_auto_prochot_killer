466543089c77d7fe2bf018bac186f8b566b6ada6
Anderson Mendes <anderson2000ano@gmail.com>
2018-10-04 10:33:28 -0300

    install msr-tools

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ echo "installing prochot killer service"
 sudo cp system/kill.prochot.service /etc/systemd/system/kill.prochot.service
 sudo cp bin/prochot.sh /usr/local/bin/prochot.sh
 sudo chmod 777 /usr/local/bin/prochot.sh
+sudo apt-get install msr-tools
 sudo systemctl daemon-reload
 sudo systemctl enable kill.prochot.service
 sudo systemctl start kill.prochot.service
