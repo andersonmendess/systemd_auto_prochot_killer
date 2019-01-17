2598a61aa0cf3e97e1f21d8de1c8de586d888214
Anderson Mendes <anderson2000ano@gmail.com>
2019-01-16 22:55:26 -0200

    cleanups && updates
    * remove the pstate limiter
    * remove unused register reader
    * add support to restore after suspend
    * add a basic detector to install msr-tools properly

--- setup.sh
+++ setup.sh
@@ -1,9 +1,29 @@
 #!/bin/sh
+
+os=`cat /etc/os-release | grep 'ID_LIKE' | sed "s/ID_LIKE=//"`
+
+echo "Detecting SO"
+sleep 1s
+if [ $os == "debian" ]
+	then
+		echo "Debian Detected..."
+		sleep 2s
+		sudo apt-get install msr-tools -y
+elif [ $os == "arch" ]
+	then
+		echo "Arch Detected..."
+		sleep 2s
+		sudo pacman -S msr-tools -y
+else
+	echo "WARN: unknown SO... Please install the msr-tools package manually"
+fi
+
+sleep 2s
+
 echo "installing prochot killer service"
 sudo cp system/kill.prochot.service /etc/systemd/system/kill.prochot.service
 sudo cp bin/prochot.sh /usr/local/bin/prochot.sh
 sudo chmod 777 /usr/local/bin/prochot.sh
-sudo apt-get install msr-tools
 sudo systemctl daemon-reload
 sudo systemctl enable kill.prochot.service
 sudo systemctl start kill.prochot.service
